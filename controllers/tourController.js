91f5749090d14ccee5feb79540d9fb7979b68306
Piotr <p.chudalewski@gmail.com>
2023-02-19 17:31:45 +0100

    API - pagination implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,14 +33,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTRE QUERY
     const tours = await query;
-
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
 
     // SEND RESPONSE
     res.status(200).json({
@@ -54,7 +60,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: [err, 'test'],
+      message: err,
     });
   }
 };

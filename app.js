ef3dc97d9fbbf1138ea3990be34eb47b8593b4c6
Piotr <p.chudalewski@gmail.com>
2023-02-21 18:30:25 +0100

    Handling unhandled routes and ndb installed

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware.🤟');
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,6 +22,13 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 // 4) START SERVER
 module.exports = app;

f1734ea8ad257efb27360c2290eb9cce25fd50ae
Piotr <p.chudalewski@gmail.com>
2023-02-24 20:19:30 +0100

    Comments removed

--- models/userModel.js
+++ models/userModel.js
@@ -3,45 +3,36 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please tell us your name!'],
-      //   unique: true,
-      //   trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, 'Please provide your email'],
-      unique: true,
-      lowercase: true,
-      validate: [validator.isEmail, 'Please provide a valid email'],
-      //   trim: true,
-    },
-    photo: String,
-    password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minlength: 8,
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, 'Please confirm your password'],
-      validate: {
-        // This only works on CREATE or SAVE!
-        validator: function (el) {
-          return el === this.password;
-        },
-        message: 'Passwords are not the same!',
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE or SAVE!
+      validator: function (el) {
+        return el === this.password;
       },
+      message: 'Passwords are not the same!',
     },
-  }
-  //   {
-  //     toJSON: { virtuals: true },
-  //     toObject: { virtuals: true },
-  //   }
-);
+  },
+});
 
 userSchema.pre('save', async function (next) {
   // Only run this func. if password was actually modified

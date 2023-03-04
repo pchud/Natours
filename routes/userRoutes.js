422c135497346f797dccfcf5725a467e80ef8daa
Piotr <p.chudalewski@gmail.com>
2023-03-04 21:06:06 +0100

    Updating current user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,10 +9,11 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch(
-  '/updatePassword',
+  '/updateMyPassword',
   authController.protect,
   authController.updatePassword
 );
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router
   .route('/')

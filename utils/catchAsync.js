063e89c7a4228327145f9cfe65b2a1eef9be5f9a
Piotr <p.chudalewski@gmail.com>
2023-03-05 14:55:18 +0100

    Implementing rate limiting for API

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,6 @@
 module.exports = fn => {
   return (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
     fn(req, res, next).catch(next); // same .catch(err => next(err))
   };
 };

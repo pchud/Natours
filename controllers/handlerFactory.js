3e70a2d6b50fa27e1c17317d33ee0987d53473ee
Piotr <p.chudalewski@gmail.com>
2023-03-22 19:09:25 +0100

    Improving read performence with indexes (mongodb)

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -72,6 +72,7 @@ exports.getAll = Model =>
       .paginate()
       .sort();
     const doc = await features.query;
+    // const doc = await features.query.explain();
 
     // SEND RESPONSE
     res.status(200).json({

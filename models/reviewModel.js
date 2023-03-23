3f88212ca0e0801894cf96579af8e8dbbed61e68
Piotr <p.chudalewski@gmail.com>
2023-03-23 20:39:08 +0100

    Calculating avg ratings on tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -51,6 +51,27 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    // {
+    //   $group: {
+    //     _id: '$tour',
+    //     nRatings: { $sum: 1 },
+    //     avgRating: { $avg: '$rating' },
+    //   },
+    // },
+  ]);
+  console.log(stats);
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;

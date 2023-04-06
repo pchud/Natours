90eaf69788fd86eef8ff670dae609f6aba6aa976
Piotr <p.chudalewski@gmail.com>
2023-04-06 22:17:52 +0200

    adding reviews to tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -16,14 +17,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res) => {
-  console.log(req.params.id);
-  const tour = await Tour.findById(req.params.id);
-  console.log(await tour);
+  //   console.log(req.params);
+  const tour = await Tour.findOne({ slug: req.params.slug });
+  const reviews = await Review.find({ tour: tour._id });
+  //   const reviews
+  console.log(await reviews);
   // 1) Get the data, for the requested tour (incl. reviews and guides)
   // 2) Build template
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: tour.name,
     tour,
+    reviews,
   });
 });

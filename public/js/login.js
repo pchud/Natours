c3e5eb781070876f3b1729bbbc3c2bd7148bcaa0
Piotr Chudalewski <p.chudalewski@gmail.com>
2023-04-11 21:27:14 +0200

    modified:   public/js/login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,20 @@
 /* eslint-disable */
-const login = (email, password) => {
-  axios({
-    method: 'POST',
-    url: '',
-  });
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', e => {

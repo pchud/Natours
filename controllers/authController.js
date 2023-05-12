fcbac0ba80ab4d8fa97154e5660eec8c64945bc9
Piotr Chudalewski <p.chudalewski@gmail.com>
2023-05-12 21:58:55 +0200

    isLoggedMethod added to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,6 +79,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
   console.log(`token: ${token}`);
 
@@ -108,6 +110,33 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // GRANT ACCESSS TO PROTECTED ROUTE
   req.user = currentUser;
+  next();
+});
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    next();
+  }
   next();
 });
 
